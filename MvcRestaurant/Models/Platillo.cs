8ef273a1ca36472bf85a279503f525bb64b60402
A440021970 <A440021970@my.uvm.edu.mx>
2022-02-04 01:04:10 -0600

    Add project files.

---
+++ MvcRestaurant/Models/Platillo.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcRestaurant.Models
+{
+    public class Platillo
+    {
+        public int idMenu { get; set; }
+        public string? NombreDelPlatillo { get; set; }
+
+        [DataType(DataType.Date)]
+        public TimeOnly TiempoDePreparacion { get; set; }
+        public string? Ingredientes { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
